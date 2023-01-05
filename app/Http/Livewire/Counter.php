87bc9ce28fd91bc76df129c7cd2416ef36634abc
Jugal Das <jugalrons@gmail.com>
2023-01-06 05:40:23 +0600

    LMS

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Flasher\Prime\FlasherInterface;
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 0;
+
+    public function render()
+    {
+        return view('livewire.counter');
+    }
+
+    public function increase (FlasherInterface $flasher, $value){
+        
+        $flasher->addSuccess('You have added successfully ' . $value);
+        $this->count++;
+        
+        
+    }
+}
